cc6197e82f89781bcad8b3d3e5f10416e55a7d79
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2022-10-25 15:18:11 +0200

    Updates

---
+++ component---src-pages-index-js-3f623955261061806995.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,a){a.r(t),a.d(t,{Head:function(){return r}});var u=a(7294),n=a(9595),r=n.Head;t.default=function(){return u.createElement(n.default,null)}}}]);
+//# sourceMappingURL=component---src-pages-index-js-3f623955261061806995.js.map
