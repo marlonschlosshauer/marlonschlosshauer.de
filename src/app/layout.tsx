3ac5ad698ec3fda76ee11c0569dabdf67af1e12c
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-09-06 16:13:30 +0200

    Format files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import { PropsWithChildren } from "react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 export default function RootLayout({ children }: PropsWithChildren) {
-  return (
-    <html lang="en" className={inter.className}>
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en" className={inter.className}>
+            <body>{children}</body>
+        </html>
+    );
 }
