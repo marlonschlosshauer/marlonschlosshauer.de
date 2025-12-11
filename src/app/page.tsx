6875c83c2969b4231e17dd126510946ec79c7a07
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-12-11 23:19:52 +0100

    Upsy all the links did not open in a new tab haha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,9 @@ export default async function Page() {
         <div className="flex flex-col gap-8">
             <h1 className="font-semibold text-xl">
                 Design Engineering + Pareto Principle ={" "}
-                <Link href="https://github.com/marlonschlosshauer">Marlon</Link>
+                <Link inNewTab href="https://github.com/marlonschlosshauer">
+                    Marlon
+                </Link>
             </h1>
             <p className="text-white-alt">
                 I&lsquo;m a full-stack developer that loves to code, design and optimise products
@@ -24,24 +26,34 @@ export default async function Page() {
             </p>
             <p className="text-white-alt">
                 After finishing my{" "}
-                <Link href="https://github.com/marlonschlosshauer/thesis">bachelors thesis</Link> in
-                applied computer science I joined{" "}
-                <Link href="https://www.figma.com/design/Wg2c9sk6nqbeWrydDOxCMg/Portfolio--Tippinho?node-id=0-1&t=euCBXXiFDOqdV9dh-1">
+                <Link inNewTab href="https://github.com/marlonschlosshauer/thesis">
+                    bachelors thesis
+                </Link>{" "}
+                in applied computer science I joined{" "}
+                <Link
+                    inNewTab
+                    href="https://www.figma.com/design/Wg2c9sk6nqbeWrydDOxCMg/Portfolio--Tippinho?node-id=0-1&t=euCBXXiFDOqdV9dh-1">
                     Tippinho
                 </Link>{" "}
                 as their app developer and designer. During my time there I also worked on{" "}
-                <Link href="https://www.figma.com/design/vbdX85EQ4ALcO1Txeeu4do/Portfolio--Neo-Strom?node-id=0-1&t=qyTLYo9BRQ5HifD8-1">
+                <Link
+                    inNewTab
+                    href="https://www.figma.com/design/vbdX85EQ4ALcO1Txeeu4do/Portfolio--Neo-Strom?node-id=0-1&t=qyTLYo9BRQ5HifD8-1">
                     Neo Strom
                 </Link>
                 , filling a technical and design role. I later took on a full-time position as a
                 frontend developer at <Link href="https://becklyn.com">Becklyn</Link>, building CMS
                 powered websites. While working full-time I also worked on{" "}
-                <Link href="https://www.figma.com/design/KgLQExaQWOnNiDD7zsw6e9/Portfolio--Secondwave?node-id=0-1&t=jy94MQDD9AeEXfUE-1">
+                <Link
+                    inNewTab
+                    href="https://www.figma.com/design/KgLQExaQWOnNiDD7zsw6e9/Portfolio--Secondwave?node-id=0-1&t=jy94MQDD9AeEXfUE-1">
                     Secondwave
                 </Link>{" "}
                 as a designer and developer on the side. Afterwards I did a redesign of Tippinho,
                 called{" "}
-                <Link href="https://www.figma.com/design/Wg2c9sk6nqbeWrydDOxCMg/Portfolio--Tippinho?node-id=6-5924&t=VgC4hyVQZblLkrTo-4">
+                <Link
+                    inNewTab
+                    href="https://www.figma.com/design/Wg2c9sk6nqbeWrydDOxCMg/Portfolio--Tippinho?node-id=6-5924&t=VgC4hyVQZblLkrTo-4">
                     Golero
                 </Link>
                 .
