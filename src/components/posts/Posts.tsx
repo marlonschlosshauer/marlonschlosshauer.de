96de61745c08d21d288cf43a94404143506b5c75
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2026-03-21 21:54:06 +0100

    Highlight my favorite posts

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,3 +1,4 @@
+import { Star } from "@/icons/Star";
 import { getBlogPosts } from "@/lib/mdx";
 import { Link } from "../shared/link/Link";
 
@@ -5,14 +6,17 @@ export const Posts = async () => {
     const metadatas = await getBlogPosts();
 
     return (
-        <ul className="list-disc list-inside">
+        <ul>
             {metadatas
                 .sort((a, b) => new Date(a.date).valueOf() - new Date(b.date).valueOf())
                 .toReversed()
                 .map((post, key) => (
-                    <li key={key}>
-                        <Link href={post.href}>
+                    <li key={key} className="flex">
+                        <Link href={post.href} className="flex flex-row items-center gap-1">
                             {new Date(post.date).getFullYear()} {post.title}
+                            {post.favorite && (
+                                <Star aria-hidden className="w-[12px] h-[12px] text-primary-alt" />
+                            )}
                         </Link>
                     </li>
                 ))}
