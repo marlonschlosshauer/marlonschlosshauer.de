61d0b28b060a5a1f14ccaf5012f0098abf1ea7fd
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2026-05-05 20:37:55 +0200

    Add subtle gradient to top and bottom

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -8,6 +8,7 @@ export default function BlogLayout({ children }: PropsWithChildren) {
             <footer>
                 <Socials />
             </footer>
+            <span className="p-8" />
         </div>
     );
 }
