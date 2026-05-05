b6267f53e097a5fcfd4ac7409ccefb0fc65abd61
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2026-05-05 20:17:00 +0200

    Add share button

--- src/components/blog/meta/Meta.tsx
+++ src/components/blog/meta/Meta.tsx
@@ -3,13 +3,17 @@ import clsx from "clsx";
 import { PropsWithClassName } from "@/types/style";
 import { AuthorDate, AuthorDateProps } from "./author-date/AuthorDate";
 import { ReadTime, ReadTimeProps } from "./read-time/ReadTime";
+import { ShareButton } from "./share-button/ShareButton";
 
 export type MetaProps = ReadTimeProps & AuthorDateProps;
 
 export const Meta: React.FC<PropsWithClassName<MetaProps>> = ({ date, words, className }) => (
-    <aside className={clsx("flex gap-2 text-white selection:text-white", className)}>
+    <aside className={clsx("flex gap-2 text-white-alt selection:text-white", className)}>
         <AuthorDate date={date} />
         <span aria-hidden>|</span>
         <ReadTime words={words} />
+        <div className="ml-auto">
+            <ShareButton />
+        </div>
     </aside>
 );
---
+++ src/components/blog/meta/share-button/ShareButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { FC, useRef, useState } from "react";
+
+export const ShareButton: FC = () => {
+    const [hasCopied, setHasCopied] = useState(false);
+    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+    const onShare = async () => {
+        const l = new URL(window.location.toString());
+        const text = l.origin + l.pathname;
+
+        const type = "text/plain";
+        const clipboardItemData = {
+            [type]: text,
+        };
+        const clipboardItem = new ClipboardItem(clipboardItemData);
+        await navigator.clipboard.write([clipboardItem]);
+
+        setHasCopied(true);
+
+        if (timeoutRef.current) {
+            return;
+        }
+
+        const t = setTimeout(() => {
+            setHasCopied(false);
+            timeoutRef.current = null;
+        }, 2000);
+
+        timeoutRef.current = t;
+    };
+
+    return <button onClick={onShare}>{hasCopied ? "Copied!" : "Share"}</button>;
+};
