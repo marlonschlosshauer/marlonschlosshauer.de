b7c07b0fe5ef76850384bbc7f59dee6dde0353f9
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2026-05-07 23:22:08 +0200

    Add some interactivity (:

--- src/components/blog/meta/author-date/AuthorDate.tsx
+++ src/components/blog/meta/author-date/AuthorDate.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useMemo, useState, useSyncExternalStore } from "react";
 
 export interface AuthorDateProps {
     date: string;
 }
 
-export const AuthorDate: React.FC<AuthorDateProps> = ({ date }) => (
-    <p aria-label={`Published on ${date}`}>{date}</p>
-);
+const formatDate = (date: string) =>
+    new Date(date).toLocaleDateString(undefined, {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
+export const AuthorDate: React.FC<AuthorDateProps> = ({ date }) => {
+    const [showLocalTime, setShowLocalTime] = useState(false);
+
+    const onClick = () => setShowLocalTime(s => !s);
+
+    return (
+        <button onClick={onClick} className="cursor-pointer">
+            {showLocalTime ? (
+                <p aria-label={`Published on ${formatDate(date)}`}>{formatDate(date)}</p>
+            ) : (
+                <p aria-label={`Published on ${date}`}>{date}</p>
+            )}
+        </button>
+    );
+};
--- src/components/blog/meta/read-time/ReadTime.tsx
+++ src/components/blog/meta/read-time/ReadTime.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 export interface ReadTimeProps {
     words: number;
 }
 
-export const ReadTime: React.FC<ReadTimeProps> = ({ words }) => (
-    <p>{Math.ceil(words / 200)} min read</p>
-);
+export const ReadTime: React.FC<ReadTimeProps> = ({ words }) => {
+    const [showInSeconds, setShowInSeconds] = useState(false);
+
+    const onClick = () => setShowInSeconds(s => !s);
+
+    return (
+        <button onClick={onClick} className="cursor-pointer">
+            {showInSeconds
+                ? `${Math.floor(Math.ceil(words / 200) * 60)} sec read`
+                : `${Math.ceil(words / 200)} min read`}
+        </button>
+    );
+};
